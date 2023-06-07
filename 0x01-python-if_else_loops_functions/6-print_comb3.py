68fbe86df5c05c43348d91ca91e80e156737e674
yasminaa12 <yasminabendriss1@gmail.com>
2023-06-06 18:06:30 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(0, 8):
-    for j in range(i + 1, 10):
-        print("{}{}".format (i, j), end=', ')
-print("{}{}".format (i + 1, j))
+for a in range(0, 8):
+    for j in range(a + 1, 10):
+        print("{}{}".format(a, j), end=', ')
+print("{}{}".format(a + 1, j))
