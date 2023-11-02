152157e346eb5cab8523a69a48c2067124a36574
yasminaa12 <yasminabendrissl@gmail.com>
2023-11-02 14:18:16 -0700

    101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -29,18 +29,3 @@ def isvalid(row, col, result):
     if row in rows or col in cols or row+col in diag1 or col-row in diag2:
         return False
     return True
-
-if __name__ == "__main__":
-    length = len(argv)
-    if length != 2:
-        print("Usage: nqueens N")
-        exit(1)
-    if argv[1].isdigit() is False:
-        print("N must be a number")
-        exit(1)
-    N = int(argv[1])
-    if N < 4:
-        print("N must be at least 4")
-        exit(1)
-    result = []
-    place(N, 0, 0, result)
