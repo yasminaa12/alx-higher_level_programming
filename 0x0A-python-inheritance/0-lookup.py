d00906d30978bb6f7405bdbf9e82a25a1816a12f
yasminaa12 <yasminabendrissl@gmail.com>
2023-11-06 14:59:14 -0800

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Returns the list of available attributesand methods of an object"""
+
+
+def lookup(obj):
+    """looks out for all attributes and methods of an object"""
+
+    return dir(obj)
