669a86b3ea190d5b18bb37b3ff518a3ed00ae799
yasminaa12 <yasminabendrissl@gmail.com>
2023-11-07 12:42:24 -0800

    1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module determine a file-writing (UTF8)function."""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to an UTF8 text document"""
+
+    with (filename, "w", encoding="utf-8") as f:
+        return f.write(text)
