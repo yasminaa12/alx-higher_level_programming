a7796a6b4f910fc00890f09140214a23399a69ed
yasminaa12 <yasminabendrissl@gmail.com>
2023-11-06 16:04:16 -0800

    3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""doc"""
+""" doc """
 
 
-
-def is_kid_of_class(obj, a_class):
-    """doc"""
+def is_kind_of_class(obj, a_class):
+    """ doc """
 
     return isinstance(obj, a_class)
