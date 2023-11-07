0967a19e5287c1d0c98b0f1760a9fd76e1d43bdf
yasminaa12 <yasminabendrissl@gmail.com>
2023-11-07 13:15:42 -0800

    4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""this module determines a JSON-to-object function."""
+"""This module determines a JSON-to-object function."""
 
-import json 
+import json
 
 
 def from_json_string(my_str):
-    """returns the python object protrayal of a JSON string"""
+    """Returns the Python object portrayal of a JSON string"""
     return json.loads(my_str)
