a87497f9c4a3fec58a0d7570ee0a0657f04f7be8
yasminaa12 <yasminabendrissl@gmail.com>
2023-11-07 12:48:29 -0800

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,5 +6,5 @@ def write_file(filename="", text=""):
     """Writes a string to an UTF8 text document"""
 
 
-    with open (filename, "w", encoding="utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
