6c0ce57d150fe00355bf232dff82f2a9129cc1f1
yasminaa12 <yasminabendrissl@gmail.com>
2023-11-07 14:12:50 -0800

    101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -13,7 +13,6 @@ def print_stats(size, status_codes):
 if __name__ == "__main__":
     import sys
 
-
     size = 0
     status_codes = {}
     valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
