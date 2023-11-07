0b2d3b1e70cc16e3687907dbd996ec7540e0a1d3
yasminaa12 <yasminabendrissl@gmail.com>
2023-11-07 13:40:46 -0800

    8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a Python (class-to-JSON) function"""
+
+
+def class_to_json(obj):
+    """returns the dictionary description of a simple data structure"""
+    return obj.__dict__#!/usr/bin/python3
+"""Defines a Python (class-to-JSON) function"""
+
+
+def class_to_json(obj):
+    """returns the dictionary description of a simple data structure"""
+    return obj.__dict__#!/usr/bin/python3
+"""Defines a Python (class-to-JSON) function"""
+
+
+def class_to_json(obj):
+    """returns the dictionary description of a simple data structure"""
+    return obj.__dict__
