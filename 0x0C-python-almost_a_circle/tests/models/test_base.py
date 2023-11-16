1b8f607b2ac254218b844588cdd7b079d6cc2845
yasminaa12 <yasminabendriss1@gmail.com>
2023-11-16 13:29:36 -0800

    __init__.py

---
+++ 0x0C-python-almost_a_circle/tests/models/test_base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Unittest for base class"""
+
+import unittest
+from models.base import Base
+
+class TestBase(unittest.TestCase):
+    """class for test """
+    def setUp(self):
+        print("setUp")
+        self.n = 10
+    def tearDown(self):
+        print("tearDown")
+        del self.n
+    @classmethod
+    def setUpClass(cls):
+        print("setUpClass")
+    @classmethod
+    def tearDownClass(cls):
+        print("tearDownClass")
+    def test_fib_assert_equal(self):
+        self.assertEqual(fib(self.n), 55)
+    def test_fib_assert_true(self):
+        self.assertTrue(fib(self.n) == 55)
